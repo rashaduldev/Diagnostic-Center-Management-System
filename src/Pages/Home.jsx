fb6e47fff8f2246080064b4843fa6c7a737fa24e
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2024-03-16 14:13:08 +0600

    Navber And Footer Added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import Footer from '../Components/Homepage/Footer/Footer';
+import Navber from './../Components/Homepage/Navber/Navber';
+
+const Home = () => {
+    return (
+        <div>
+            <Navber></Navber>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
